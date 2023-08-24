b0989ccd460accd17bbbd7686e1702f04798eae5
ZakirovaVera <zakirovavk@gmail.com>
2023-08-24 11:28:02 +0500

    news-articles__blog

---
+++ dz3Maket/components/news.js
@@ -0,0 +1,15 @@
+const Counter = {
+    data() {
+        return {
+        }
+    },
+    methods: {
+        
+    },
+    template: `
+    <div>
+        <h2>{{counter}}</h2>
+        <button @click="add">+</button>
+        <button @click="minus">-</button>
+    </div>`,
+}
