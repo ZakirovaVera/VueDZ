80bc280d2b1c9b97ab4fa842fa12609bf32c10bb
ZakirovaVera <zakirovavk@gmail.com>
2023-08-30 12:37:57 +0500

    создан и вынесен отдельно header, footer component

--- dz4/vue-dz4/src/App.vue
+++ dz4/vue-dz4/src/App.vue
@@ -1,13 +1,21 @@
 <template>
+  <HeaderComponent/>
   <OneMaketComponent />
+  <FooterComponent/>
+  <HeaderComponent/>
   <TwoMaketComponent />
+  <FooterComponent/>
+  <HeaderComponent/>
   <ThreeMaketComponent/>
+  <FooterComponent/>
 </template>
 
 <script>
 import OneMaketComponent from './components/OneMaketComponent.vue';
 import TwoMaketComponent from './components/TwoMaketComponent.vue';
 import ThreeMaketComponent from './components/ThreeMaketComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue'
 
 export default {
   name: 'App',
@@ -15,8 +23,25 @@ export default {
     OneMaketComponent,
     TwoMaketComponent,
     ThreeMaketComponent,
-  }
+    HeaderComponent,
+    FooterComponent,
+}
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "@/assets/_vars.scss" ;
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.center {
+    padding-left: calc(50% - $siteWidth / 2);
+    padding-right: calc(50% - $siteWidth / 2);
+}
+
+a {
+    text-decoration: none;
+}</style>
---
+++ dz4/vue-dz4/src/components/HeaderComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <header class="header center">
+        <div class="header__left">
+            <a class="header__left__logo" href="index.html">
+                <img src="@/assets/img/Logo.svg" alt="logotip">
+            </a>
+            <h1 class="header__left__logo-text">Interno</h1>
+        </div>
+        <div class="header__right">
+            <a class="header__menu" href="index.html">Home</a>
+            <a class="header__menu" href="#">Project</a>
+            <a class="header__menu" href="#">Blog</a>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name:"HeaderComponent"
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/_vars.scss" ;
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 53px;
+    padding-bottom: 57px;
+
+    &__left {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        gap: 10px;
+        padding-left: 4px;
+
+        &__logo-text {
+            color: $siteColorHeading;
+            font-size: 40px;
+            font-weight: 400;
+            line-height: 50px;
+            word-wrap: break-word;
+            font-family: DM Serif Display;
+        }
+    }
+
+    &__right {
+        display: flex;
+        gap: 30px;
+        padding-right: 4px;
+    }
+
+    &__menu {
+        color: $siteColorHeading;
+        text-align: center;
+        font-family: Jost;
+        font-size: 20px;
+        line-height: 25px;
+        font-style: normal;
+        font-weight: 400;
+    }
+}
+</style>
