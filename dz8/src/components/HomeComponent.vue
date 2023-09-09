0e0ad6105275f5aa498bb3f21af7da9c0d885c07
ZakirovaVera <zakirovavk@gmail.com>
2023-09-09 20:07:07 +0500

    dz8 pages - NFC и редакт HomeComponent правка banner__button

--- dz8/src/components/HomeComponent.vue
+++ dz8/src/components/HomeComponent.vue
@@ -4,7 +4,7 @@
             <h1 class="banner__heading">Let Your Home Be Unique</h1>
             <p class="banner__text">There are many variations of the passages of
                 lorem Ipsum fromavailable, majority.</p>
-            <router-link class="banner__button" to="/notpage">Get Started
+            <router-link class="banner__button" to="/not-page">Get Started
                 <svg width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887" stroke="#CDA274"
                         stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
